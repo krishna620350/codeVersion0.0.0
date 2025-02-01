d9217784a75e6ebbe752c6cdc7bf3eea11596c28
krishna620350 <manishkumar620350@gmail.com>
2025-02-01 19:31:07 +0530

    work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import router from "./router/router.js";
 
 const app = express();
 app.use(router);
-app.listen(5002, () => { 
-    console.log("Server is running on port 5002");
+const port = process.env.PORT || 5002;
+app.listen(port, () => { 
+    console.log(`Server is running on port ${port}`);
 });
