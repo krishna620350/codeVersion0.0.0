5b4b7ec64575138cf6f6203f8c388af8e4e941ac
krishna620350 <manishkumar620350@gmail.com>
2025-02-01 22:36:55 +0530

    work

--- router/router.js
+++ router/router.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => { 
-    res.status(200).json({message: false});
+    res.status(200).json({message: true});
 });
 
 export default router;
