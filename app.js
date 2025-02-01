babe556171c51c43375dcdd88a84286d34f7038e
krishna620350 <manishkumar620350@gmail.com>
2025-02-01 12:05:59 +0530

    work

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import express from "express";
+import router from "./router/router.js";
 
 const app = express();
-
-app.get("/", (req, res) => { 
-    res.send("Hello World!");
-});
-
+app.use(router);
 app.listen(5002, () => { 
     console.log("Server is running on port 5002");
 });
