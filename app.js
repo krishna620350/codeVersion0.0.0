3dba9d5d0c35f7ec0bca014d8e7b866bfd8122db
krishna620350 <manishkumar620350@gmail.com>
2025-02-02 12:13:33 +0530

    code

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import { PORT } from "./configuration/envConfig.js";
+// import { PORT, API } from "./configuration/envConfig.js";
+import dotenv from "dotenv";
 import express from "express";
 import usersRouter from "./router/usersRouter.js";
 
-
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use(`${process.env.API}/users`, usersRouter);
 
-const port = PORT;
+const port = process.env.PORT;
 app.listen(port, () => {
   // Log a message indicating that the server is running and listening on the specified port
   console.log(`Listening on port ${port}`);
