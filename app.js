e776a0f264f7186f0b75e9ab434173acb4a662de
krishna620350 <manishkumar620350@gmail.com>
2025-02-02 12:02:20 +0530

    code

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-// Import the express framework for building the web server
-// Import dotenv to load environment variables from a .env file
 import { PORT } from "./configuration/envConfig.js";
 import express from "express";
 import usersRouter from "./router/usersRouter.js";
-import https from "https";
-// import fs from "fs";
-// import path, { dirname} from "path";
-// import { fileURLToPath } from "url";
 
-// Create an instance of an express application
+
 const app = express();
 app.use(express.json());
-// Load environment variables from the .env file into process.env
+
 app.use(`${process.env.API}/users`, usersRouter);
 
-// Set the port to the value of the PORT environment variable or default to 3000 if not specified
-const port = PORT || 5000;
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// const httpsOption = {
-//   key: fs.readFileSync(path.join(__dirname, "web", "cert-key.pem")),
-//   cert: fs.readFileSync(path.join(__dirname, "web", "cert.pem")),
-// };
-
-// // Create HTTPS server using the options
-// const httpsServer = https.createServer(httpsOption, app);
-
-// Start the server and listen on the specified port (using httpsServer)
+const port = PORT;
 app.listen(port, () => {
   // Log a message indicating that the server is running and listening on the specified port
   console.log(`Listening on port ${port}`);
--- configuration/envConfig.js
+++ configuration/envConfig.js
@@ -5,4 +5,4 @@ export const API = process.env.API;
 export const API_RATE_LIMIT_TIME = process.env.API_RATE_LIMIT_TIME;
 export const API_RATE_LIMIT_REQUEST = process.env.API_RATE_LIMIT_REQUEST;
 export const API_RATE_LIMIT_HEADERS = process.env.API_RATE_LIMIT_HEADERS;
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 5000;
